793751214b947914932585f12dc7016ef06ca499
Abed El-Rahim Badr <abedbadr1999@hotmail.com>
2019-11-04 00:24:00 +0100

    Installed swagger nugget packet and configured the REST service to use it

--- RestItemService/Startup.cs
+++ RestItemService/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace RestItemService
 {
@@ -26,6 +27,7 @@ namespace RestItemService
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new Info {Title = "Items API", Version = "v1.0"}); });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +43,10 @@ namespace RestItemService
             }
 
             app.UseHttpsRedirection();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Items API v1.0"));
+
             app.UseMvc();
         }
     }
